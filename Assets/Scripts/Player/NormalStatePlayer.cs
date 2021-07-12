27de13ef5934d1e8d043af48bfe60fe16c8fcd25
redd096 <martin.babino96@gmail.com>
2021-07-12 10:59:25 +0200

    inserito player che si muove + gun sprite

---
+++ Assets/Scripts/Player/NormalStatePlayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using redd096;
+
+public class NormalStatePlayer : StateMachineBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] float speed = 5;
+
+    Player player;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+
+        //get references
+        player = animator.GetComponent<Player>();
+    }
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateUpdate(animator, stateInfo, layerIndex);
+
+        //move
+        Movement(InputRedd096.GetValue<Vector2>("Move"));
+    }
+
+    void Movement(Vector2 inputMovement)
+    {
+        //set velocity
+        player.Rb.velocity = inputMovement.normalized * speed;
+    }
+}
---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D Rb { get; private set; }
+
+    private void Awake()
+    {
+        Rb = GetComponent<Rigidbody2D>();
+    }
+}
