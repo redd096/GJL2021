dba712bd339d30323b841f97bb6bfee907a49c1e
redd096 <martin.babino96@gmail.com>
2021-07-12 10:27:58 +0200

    inserito new input system + script vari

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,29 @@
+﻿namespace redd096
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    [AddComponentMenu("redd096/MonoBehaviours/UI Manager")]
+    public class UIManager : MonoBehaviour
+    {
+        [Header("Menu")]
+        [SerializeField] GameObject pauseMenu = default;
+
+        void Start()
+        {
+            //by default, deactive pause menu
+            PauseMenu(false);
+        }
+
+        public void PauseMenu(bool active)
+        {
+            if (pauseMenu == null)
+            {
+                return;
+            }
+
+            //active or deactive pause menu
+            pauseMenu.SetActive(active);
+        }
+    }
+}
---
+++ Assets/Scripts/redd096/Singletons/GameManager.cs
@@ -0,0 +1,17 @@
+﻿namespace redd096
+{
+    using UnityEngine;
+
+    [AddComponentMenu("redd096/Singletons/Game Manager")]
+    [DefaultExecutionOrder(-100)]
+    public class GameManager : Singleton<GameManager>
+    {
+        public UIManager uiManager { get; private set; }
+
+        protected override void SetDefaults()
+        {
+            //get references
+            uiManager = FindObjectOfType<UIManager>();
+        }
+    }
+}
