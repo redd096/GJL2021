29933418687b6e19f3027cdb69c8490985526515
redd096 <martin.babino96@gmail.com>
2021-07-14 12:25:11 +0200

    creato prefab prop

---
+++ Assets/Scripts/Feedbacks/DestructiblePropsFeedbacks.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructiblePropsFeedbacks : MonoBehaviour
+{
+    DestructibleProps props;
+
+    void OnEnable()
+    {
+        //get references
+        props = GetComponent<DestructibleProps>();
+
+        //add events
+        if(props)
+        {
+            props.onGetDamage += OnGetDamage;
+            props.onDie += OnDie;
+        }
+    }
+
+    void OnDisable()
+    {
+        //remove events
+        if (props)
+        {
+            props.onGetDamage += OnGetDamage;
+            props.onDie += OnDie;
+        }
+    }
+
+    void OnGetDamage()
+    {
+        //TODO
+        //blinka
+    }
+
+    void OnDie()
+    {
+        //TODO
+        //animazione di esplosione
+    }
+}
